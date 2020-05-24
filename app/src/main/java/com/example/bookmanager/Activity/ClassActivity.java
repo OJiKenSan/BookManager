52dc0b10238acb687b865977e296519b5832e02a
王子轩 <609247863@qq.com>
2020-05-24 15:39:43 +0800

    loginChange

---
+++ app/src/main/java/com/example/bookmanager/Activity/ClassActivity.java
@@ -0,0 +1,18 @@
+package com.example.bookmanager.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.bookmanager.R;
+
+public class ClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class);
+    }
+}
